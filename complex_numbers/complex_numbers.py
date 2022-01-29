7021b2032a298920f58abf484aafcb96c13ac8d2
TheCatSaber <alexmylet2@gmail.com>
2022-01-29 19:14:51 +0000

    1.2.1

--- complex_numbers/complex_numbers.py
+++ complex_numbers/complex_numbers.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import math
+
 
 class ComplexNumber:
     _re: float
@@ -28,11 +30,36 @@ class ComplexNumber:
             return self.get_real() == other.get_real() and self.get_imaginary() == other.get_imaginary()  # type: ignore
 
     @staticmethod
+    def modulus(c: ComplexNumber) -> float:
+        return math.sqrt(ComplexNumber.modulus_squared(c))
+
+    @staticmethod
+    def modulus_squared(c: ComplexNumber) -> float:
+        return (c._re * c._re) + (c._im * c._im)
+
+    @staticmethod
+    def conjugate(c: ComplexNumber) -> ComplexNumber:
+        return ComplexNumber(c._re, -c._im)
+
+    @staticmethod
     def add(c1: ComplexNumber, c2: ComplexNumber) -> ComplexNumber:
         return ComplexNumber(c1._re + c2._re, c1._im + c2._im)
+
+    @staticmethod
+    def subtract(c1: ComplexNumber, c2: ComplexNumber) -> ComplexNumber:
+        return ComplexNumber(c1._re - c2._re, c1._im - c2._im)
 
     @staticmethod
     def multiply(c1: ComplexNumber, c2: ComplexNumber) -> ComplexNumber:
         new_real: float = (c1._re * c2._re) - (c1._im * c2._im)
         new_imaginary: float = (c1._im * c2._re) + (c1._re * c2._im)
+        return ComplexNumber(new_real, new_imaginary)
+
+    @staticmethod
+    def divide(c1: ComplexNumber, c2: ComplexNumber) -> ComplexNumber:
+        modulus_squared = ComplexNumber.modulus_squared(c2)
+        if modulus_squared == 0:
+            raise ValueError("c2 cannot have a modulus_squared of 0.")
+        new_real: float = ((c1._re * c2._re) + (c1._im * c2._im)) / modulus_squared
+        new_imaginary: float = ((c2._re * c1._im) - (c1._re * c2._im)) / modulus_squared
         return ComplexNumber(new_real, new_imaginary)
--- tests/test_complex_numbers.py
+++ tests/test_complex_numbers.py
@@ -18,31 +18,90 @@ class ComplexNumberInitCheck(unittest.TestCase):
 
     def test___eq___same(self):
         self.assertTrue(ComplexNumber(1, 1) == ComplexNumber(1, 1))
-    
+
     def test___eq___not_same_re(self):
         self.assertFalse(ComplexNumber(1, 1) == ComplexNumber(0, 1))
-    
+
     def test___eq___not_same_im(self):
         self.assertFalse(ComplexNumber(1, 1) == ComplexNumber(1, 2))
 
     def test___eq___not_same_both(self):
         self.assertFalse(ComplexNumber(1, 1) == ComplexNumber(0, -1))
-    
+
     def test___eq___wrong_type(self):
         self.assertFalse(ComplexNumber(1, 1) == 1)
 
-class ComplexNumberAddCheck(unittest.TestCase):
 
+class ComplexNumberAddCheck(unittest.TestCase):
     def test_add_positive(self):
-        self.assertEqual(ComplexNumber.add(ComplexNumber(1, 1), ComplexNumber(3, 2)), ComplexNumber(4, 3))
-    
+        self.assertEqual(
+            ComplexNumber.add(ComplexNumber(1, 1), ComplexNumber(3, 2)),
+            ComplexNumber(4, 3),
+        )
+
     def test_add_negative_re(self):
-        self.assertEqual(ComplexNumber.add(ComplexNumber(1, 1), ComplexNumber(-2, 0)), ComplexNumber(-1, 1))
+        self.assertEqual(
+            ComplexNumber.add(ComplexNumber(1, 1), ComplexNumber(-2, 0)),
+            ComplexNumber(-1, 1),
+        )
+
+
+class ComplexNumberSubtractCheck(unittest.TestCase):
+    def test_subtract_positive(self):
+        self.assertEqual(
+            ComplexNumber.subtract(ComplexNumber(3, 3), ComplexNumber(3, 2)),
+            ComplexNumber(0, 1),
+        )
+
 
 class ComplexNumberMultiplyCheck(unittest.TestCase):
-
     def test_multiply_i_i(self):
-        self.assertEqual(ComplexNumber.multiply(ComplexNumber(0, 1), ComplexNumber(0, 1)), ComplexNumber(-1, 0))
+        self.assertEqual(
+            ComplexNumber.multiply(ComplexNumber(0, 1), ComplexNumber(0, 1)),
+            ComplexNumber(-1, 0),
+        )
+
+
+class ComplexNumberDivideCheck(unittest.TestCase):
+    def test_divide_book_example(self):
+        self.assertEqual(
+            ComplexNumber.divide(ComplexNumber(-2, 1), ComplexNumber(1, 2)),
+            ComplexNumber(0, 1),
+        )
+
+    def test_throw_value_error(self):
+        self.assertRaises(
+            ValueError, ComplexNumber.divide, ComplexNumber(-2, 1), ComplexNumber(0, 0)
+        )
+
+
+class ComplexNumberModulusCheck(unittest.TestCase):
+    def test_modulus_zero(self):
+        self.assertEqual(ComplexNumber.modulus(ComplexNumber(0, 0)), 0)
+
+    def test_modulus_3_4_5(self):
+        self.assertEqual(ComplexNumber.modulus(ComplexNumber(3, 4)), 5)
+
+    def test_modulus_4_3_5(self):
+        self.assertEqual(ComplexNumber.modulus(ComplexNumber(4, 3)), 5)
+
+
+class ComplexNumberConjugateCheck(unittest.TestCase):
+    def test_conjugate_pos_neg(self):
+        self.assertEqual(
+            ComplexNumber.conjugate(ComplexNumber(1, 1)), ComplexNumber(1, -1)
+        )
+
+    def test_conjugate_neg_pos(self):
+        self.assertEqual(
+            ComplexNumber.conjugate(ComplexNumber(1, -2)), ComplexNumber(1, 2)
+        )
+
+    def test_conjugate_zero(self):
+        self.assertEqual(
+            ComplexNumber.conjugate(ComplexNumber(2, 0)), ComplexNumber(2, 0)
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
